23de1458d4c9fe11605a690d156bc5bb83e9b84d
dodger213 <alenschmitt415@gmail.com>
2024-01-26 15:30:25 +1100

    Update the Search and pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -14,7 +14,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const currentPage = Number(searchParams.get('page')) || 1;
   const allPages = generatePagination(currentPage, totalPages);
 
-
   const createPageURL = (pageNumber:number | string) =>{
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber?.toString());
