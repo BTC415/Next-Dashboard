9d004c722a1cc6563995aad386372a383f990052
dodger213 <alenschmitt415@gmail.com>
2024-01-29 17:12:43 +1100

    Modified SideNav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -5,6 +5,7 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 
 export default function SideNav() {
+  
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
